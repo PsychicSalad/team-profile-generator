369f5538fcf2cc97f1ecc7f77cff497ae1dfd239
PsychicSalad <54758143+PsychicSalad@users.noreply.github.com>
2023-03-07 11:48:35 +0000

    Worked on index js file

--- index.js
+++ index.js
@@ -10,6 +10,61 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const idList = [];
+const teamMembers = [];
+
+const appMenu = () => {
+  function createManager() {
+    console.log("Please build your team!");
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the name of the team manager?",
+          validate: (answer) => {
+            if (answer !== "") {
+              return true;
+            }
+
+            return "Please enter at least one character";
+          },
+        },
+
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the team manager's ID?",
+        },
+
+        {
+          type: "input",
+          name: "managerEmail",
+          message: "What is the manager's email?",
+        },
+        {
+          type: "input",
+          name: "managerOfficeNumber",
+          message: "What is the team manager's office number?",
+        },
+      ])
+      .then((answers) => {
+        const manager = new Manager(
+          answers.managerName,
+          answers.managerId,
+          answers.managerEmail,
+          answers.managerOfficeNumber
+        );
+        teamMembers.push(manager);
+        idList.push(answers.managerId);
+
+        createTeam();
+      });
+  }
+
+  createManager();
+};
+
+appMenu();
